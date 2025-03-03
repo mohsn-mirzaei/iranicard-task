c3056619eef2d88133876547dec32f02511f809f
Mohsen <mohsn.mirzaei@gmail.com>
2025-03-03 14:51:09 +0330

    Build input-floating-label component like MUI v3 & Update login inputs

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import InputFloatingLabel from "./input-floating-label"
 
 export function LoginForm({
   className,
@@ -26,27 +27,9 @@ export function LoginForm({
         <CardContent>
           <form>
             <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="phone">شماره تلفن</Label>
-                <Input
-                  id="phone"
-                  type="tel"
-                  placeholder="09xxxxxxxxx"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">رمز عبور</Label>
-                  <a
-                    href="#"
-                    className="ms-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    رمز عبور خود را فراموش کرده اید؟
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
+                <InputFloatingLabel id="phone" label="شماره تلفن" type="tel"/>
+                <InputFloatingLabel id="password" label="رمز عبور" type="password"/>
+              
               <Button type="submit" className="w-full">
                 ورود
               </Button>
