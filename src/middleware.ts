266f6713ddb2108229e9ac85033b85c38e8bf4d2
Mohsen <mohsn.mirzaei@gmail.com>
2025-03-03 17:04:14 +0330

    Fixed type in meddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
  
 // 1. Specify protected and public routes
-const protectedRoutes = ['/wallets', 'add-wallet']
+const protectedRoutes = ['/wallets', '/add-wallet']
 const publicRoutes = ['/login', '/']
  
 export default async function middleware(req: NextRequest) {
